35a8e07efa6d433a32a142d9335c1f90ba9cb7b5
wmhou <29271004+wmhou@users.noreply.github.com>
2022-01-04 17:32:23 +0800

    mapping

--- contracts/BasicKnowledge/23-mapping映射.sol
+++ contracts/BasicKnowledge/23-mapping映射.sol
@@ -4,7 +4,18 @@ pragma solidity ^0.8.0;
 contract Demo {
     mapping(address => uint256) public balances;
 
+    mapping (string => uint256) public params;
+
     function update(uint256 newBalance) public {
         balances[msg.sender] = newBalance;
     }
+
+    function pushParam(string memory _key, uint256 _value) internal {
+        params[_key] = _value;
+    }
+
+    function getValue(string memory _key) internal view returns (uint256){
+        return params[_key];
+    }
+
 }

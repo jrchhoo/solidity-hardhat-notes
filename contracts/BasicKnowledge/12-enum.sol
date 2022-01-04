3c55cb8fb9432e8f8d86702cbf179819d65cc66b
wmhou <29271004+wmhou@users.noreply.github.com>
2022-01-04 11:18:56 +0800

    格式化

--- contracts/BasicKnowledge/12-enum.sol
+++ contracts/BasicKnowledge/12-enum.sol
@@ -1,16 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
-contract Demo{
-
+contract Demo {
     // 枚举实际上就是下标0-255的256个成员，最多不超过256个成员，最少不少于1个
-    enum Season{
-        Spring, Summer, Autumn, Winter
+    enum Season {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
     }
 
     Season public season;
 
-    constructor(Season _s){
+    constructor(Season _s) {
         season = _s;
     }
 
@@ -18,7 +20,7 @@ contract Demo{
         season = _s;
     }
 
-    function getSeason() public view returns (Season){
+    function getSeason() public view returns (Season) {
         return season;
     }
 }

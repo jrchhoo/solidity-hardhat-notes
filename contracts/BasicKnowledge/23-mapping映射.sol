4f3c03826d69657e99524a00c98396fad373d066
wmhou <29271004+wmhou@users.noreply.github.com>
2022-01-04 11:14:45 +0800

    项目结构修改

--- contracts/BasicKnowledge/23-mapping映射.sol
+++ contracts/BasicKnowledge/23-mapping映射.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 contract Demo {
     mapping(address => uint256) public balances;
 
-
     function update(uint256 newBalance) public {
         balances[msg.sender] = newBalance;
     }
